33b41f41c8c9168653d7cfd62867c2a00f2f4f25
Claus Marxmeier <claus@marxmeier.de>
2020-04-13 19:40:37 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -47,6 +47,12 @@
             "offset_version" => 4
         ),
         array(
+            "name" => "Aruba",
+            "filename" => "aruba",
+            "offset_modell" => 6,
+            "offset_version" => 4
+        ),
+        array(
             "name" => "Asus",
             "filename" => "asus",
             "offset_modell" => 4,
